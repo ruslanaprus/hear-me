79e9baf4fb2307645220cf2c75494078e625316a
ruslana <ruslana.prus@ukr.net>
2026-05-10 16:10:26 +0100

    Replaced file_create_url() with FileUrlGeneratorInterface

--- src/Plugin/Field/Formatter/TtsAudioFormatter.php
+++ src/Plugin/Field/Formatter/TtsAudioFormatter.php
@@ -2,28 +2,108 @@
 
 namespace Drupal\hear_me\Plugin\Field\Formatter;
 
+use Drupal\Core\Field\Attribute\FieldFormatter;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\File\FileUrlGeneratorInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'tts_audio_formatter' formatter.
  *
- * @FieldFormatter(
- *   id = "tts_audio_formatter",
- *   label = @Translation("TTS Audio Player"),
- *   field_types = {
- *     "file"
- *   }
- * )
+ * Renders a managed audio file field as an HTML5 <audio> element with
+ * playback controls. Intended for use with the hear_me module's Audio
+ * media type, but works with any file field containing audio.
  */
+#[FieldFormatter(
+  id: 'tts_audio_formatter',
+  label: new TranslatableMarkup('TTS Audio Player'),
+  field_types: ['file'],
+)]
 class TtsAudioFormatter extends FormatterBase {
 
-  public function viewElements(FieldItemListInterface $items, $langcode) {
+  /**
+   * The file URL generator service.
+   *
+   * @var \Drupal\Core\File\FileUrlGeneratorInterface
+   */
+  protected FileUrlGeneratorInterface $fileUrlGenerator;
+
+  /**
+   * Constructs a TtsAudioFormatter instance.
+   *
+   * @param string $plugin_id
+   *   The plugin ID for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third-party settings.
+   * @param \Drupal\Core\File\FileUrlGeneratorInterface $file_url_generator
+   *   The file URL generator service.
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    $label,
+    $view_mode,
+    array $third_party_settings,
+    FileUrlGeneratorInterface $file_url_generator,
+  ) {
+    parent::__construct(
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $label,
+      $view_mode,
+      $third_party_settings,
+    );
+
+    $this->fileUrlGenerator = $file_url_generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ): static {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('file_url_generator'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode): array {
     $elements = [];
 
     foreach ($items as $delta => $item) {
       if ($file = $item->entity) {
-        $url = file_create_url($file->getFileUri());
+        $url = $this->fileUrlGenerator->generateAbsoluteString($file->getFileUri());
         $elements[$delta] = [
           '#type' => 'html_tag',
           '#tag' => 'audio',
@@ -39,4 +119,5 @@ class TtsAudioFormatter extends FormatterBase {
 
     return $elements;
   }
+
 }
